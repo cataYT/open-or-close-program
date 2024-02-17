0c4db451a0b839397f239e89fb2b3144f0e1bb26
cataYT <firzt.superb.pro7@gmail.com>
2024-02-18 01:17:37 +0530

    created the whole program open and close

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Diagnostics;
+class MainProgram {
+    public static void Main(string[] args) {
+        Console.Write("O or C: ");
+        string? input = Console.ReadLine();
+        if (input != null) {
+            if (input.ToLower() == "o") {
+                Process.Start("C:\\Program Files\\Notepad++\\notepad++.exe");
+            } else if (input.ToLower() == "c") {
+                Process[] processes = Process.GetProcessesByName("notepad++");
+                foreach (Process process in processes) {
+                    process.Kill();
+                    process.WaitForExit();
+                    process.Dispose();
+                }
+            }
+        }
+    }
+}
